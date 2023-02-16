38e538e147455dd2e478342f921fc4a747d39fce
Vadim Barmenkov <vadimbarmenkov@gmail.com>
2023-02-16 15:19:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import './main.css';
+
+function ShopItemFunc (props){
+    return <div className="main-content">
+        <h2>{props.item.brand}</h2>
+        <h1>{props.item.title}</h1>
+        <h3>{props.item.description}</h3>
+        <div className="description">
+            {props.item.descriptionFull}
+        </div>
+        <div className="highlight-window mobile">
+            <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+            <div className="price">{props.item.currency + props.item.price}</div>
+            <button>Добавить в корзину</button>
+        </div>
+    </div>;
+}
 
 function App() {
+
+    const item = {
+        brand: 'Tiger of Sweden',
+        title: 'Leonard coat',
+        description: 'Minimalistic coat in cotton-blend',
+        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+        price: 399,
+        currency: '£'
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <ShopItemFunc item={item} />
+        </div>
+      </div>
   );
 }
 
